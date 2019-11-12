25d434e83f12497ef61a6dbf72d1a8c46a9cff99
Mashustia <denisova.mariya.dm@gmail.com>
2019-11-12 14:19:24 +0300

    Settings for Redux DevTools Extension added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,12 @@
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
 
 import {reducer} from '../reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = (typeof window !== `undefined` && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers()
+);
 
 export default store;
